0dea91a4048fb85b3a602b39037745c796ed1682
kaneki666 <msadman789@gmail.com>
2022-01-04 16:44:48 +0600

    4janss

--- app.js
+++ app.js
@@ -5,13 +5,8 @@ const fs = require("fs");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-// const PORT = 3000;
-// app.use(cors());
-// app.use(bodyParser.json());
+
 app.use(express.static(path.resolve(__dirname, "./build")));
-app.use(
-  require("prerender-node").set("prerenderToken", "FOZCAXACsPWxN56ZQzck")
-);
 
 app.get("/*", (req, res, next) => {
   const filePath = path.resolve(__dirname, "./build", "index.html");
