6e25ff44f9d8ff36ae543ce9bf6a6d0fde3532ec
kaneki666 <msadman789@gmail.com>
2022-01-04 15:23:35 +0600

    4janss

--- app.js
+++ app.js
@@ -17,21 +17,21 @@ app.get("/*", (req, res, next) => {
       console.error("Error during file reading", err);
       return res.status(404).end();
     }
-    // get post info
-    const post = req.query;
-    console.log("req");
-    console.log(post.url, post.image);
-    // const post = getPostById(postId);
-    // if (!post) return res.status(404).send("Post not found");
+    // // get post info
+    // const post = req.query;
+    // console.log("req");
+    // console.log(post.url, post.image);
+    // // const post = getPostById(postId);
+    // // if (!post) return res.status(404).send("Post not found");
 
-    // inject meta tags
-    data = data.replace("__META_OG_URL__", post.url);
-    data = data.replace("__META_OG_TITLE__", "Donee goal detail");
-    data = data.replace("__META_OG_DESCRIPTION__", "laptop");
-    data = data.replace(
-      "__META_OG_IMAGE__",
-      "https://donee.s3.amazonaws.com/media/images/goal_compressed_images/photo-1488521787991-ed7bbaae773c.jpg"
-    );
+    // // inject meta tags
+    // data = data.replace("__META_OG_URL__", post.url);
+    // data = data.replace("__META_OG_TITLE__", "Donee goal detail");
+    // data = data.replace("__META_OG_DESCRIPTION__", "laptop");
+    // data = data.replace(
+    //   "__META_OG_IMAGE__",
+    //   "https://donee.s3.amazonaws.com/media/images/goal_compressed_images/photo-1488521787991-ed7bbaae773c.jpg"
+    // );
     return res.send(data);
   });
 });
