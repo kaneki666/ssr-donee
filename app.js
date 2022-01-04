8232639cd5b31a944c33cedab4f0ed345e752c4e
kaneki666 <msadman789@gmail.com>
2022-01-04 14:37:32 +0600

    4jan

--- app.js
+++ app.js
@@ -3,52 +3,58 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// app.get("/", function (req, res) {
-//   const filePath = path.resolve(__dirname, "./public", "index.html");
+const PORT = 5000;
+// app.use(cors());
+// app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, "./build")));
+
+// app.post("/data", async (req, res) => {
+//   const d = req.body;
+//   console.log(req.body);
+//   res.send("ok");
+// });
+
+// app.get("*", function (req, res) {
+//   const filePath = path.resolve(__dirname, "./build", "index.html");
 
 //   fs.readFile(filePath, "utf8", function (err, data) {
 //     if (err) {
 //       return console.log(err);
 //     }
-
-//     data = data.replace(/{{title}}/, "Sachin Verma");
+//     console.log(req);
+//     data = data.replace("__META_OG_TITLE__", "tEST Goal 3");
+//     data = data.replace("__META_DESCRIPTION__", "Donee goal detail");
+//     data = data.replace("__META_OG_DESCRIPTION__", "laptop");
 //     data = data.replace(
-//       /{{description}}/,
-//       "Sachin Verma's personal site and blog"
+//       "__META_OG_IMAGE__",
+//       "https://donee.s3.amazonaws.com/media/images/goal_images/photo-1488521787991-ed7bbaae773c_e4nGOZ5.jpeg"
 //     );
-
 //     res.send(data);
 //   });
 // });
 
-app.use(express.static(path.resolve(__dirname, "./build")));
-
-app.get("*", function (req, res) {
-  console.log(req.body);
+app.get("/*", (req, res, next) => {
   const filePath = path.resolve(__dirname, "./build", "index.html");
-  fs.readFile(filePath, "utf8", function (err, data) {
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      return console.log(err);
+      console.error("Error during file reading", err);
+      return res.status(404).end();
     }
+    // get post info
+    const post = req.query;
 
+    // const post = getPostById(postId);
+    // if (!post) return res.status(404).send("Post not found");
+
+    // inject meta tags
+    data = data.replace("__META_OG_URL__", post.url);
     data = data.replace("__META_OG_TITLE__", "tEST Goal 3");
     data = data.replace("__META_DESCRIPTION__", "Donee goal detail");
     data = data.replace("__META_OG_DESCRIPTION__", "laptop");
-    data = data.replace(
-      "__META_OG_IMAGE__",
-      "https://donee.s3.amazonaws.com/media/images/goal_images/photo-1488521787991-ed7bbaae773c_e4nGOZ5.jpeg"
-    );
-    res.send(data);
+    data = data.replace("__META_OG_IMAGE__", post.image);
+    return res.send(data);
   });
 });
-
-{
-  /* <meta name="description" content="__META_DESCRIPTION__"/>
-<meta name="og:title" content="__META_OG_TITLE__"/>
-<meta name="og:description" content="__META_OG_DESCRIPTION__"/>
-<meta name="og:image" content="__META_OG_IMAGE__"/> */
-}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
